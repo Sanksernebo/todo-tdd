cc413f4b2e7ee03a70203dedd0fdc3fa2f94fef6
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2024-01-25 13:25:15 +0200

    Created TodoController.createTodo intergration test and fixed unit test for using database

---
+++ controllers/todo.controller.js
@@ -0,0 +1,8 @@
+const TodoModel = require('../models/todo.model')
+
+const createTodo = async (req, res, next) => {
+      const createdModel = await  TodoModel.create(req.body)
+        res.status(201).json(createdModel)
+}
+
+module.exports = {createTodo}
